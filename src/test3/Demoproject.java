ea9b5cf805e3a6c50cdab3b318ae3609a51e3dcf
varun0223 <mvarunraviteja@gmail.com>
2024-12-30 19:10:39 +0530

    initial commit

---
+++ src/test3/Demoproject.java
@@ -0,0 +1,12 @@
+package test3;
+
+public class Demoproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello i am varun");
+		
+
+	}
+
+}
